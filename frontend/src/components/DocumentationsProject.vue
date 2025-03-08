626a1bd0bfb5dd480e39c13d6ec0034573b0e21f
Batouuuuu <gillet.baptiste94@gmail.com>
2025-03-08 16:35:13 +0100

    aled

---
+++ frontend/src/components/DocumentationsProject.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="nouvelle-page">
+      <h1>Nouvelle Page</h1>
+      <p>Bienvenue sur la nouvelle page !</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "DocumentationsProject",
+  };
+  </script>
+  
+  <style scoped>
+  .nouvelle-page {
+    text-align: center;
+    padding: 20px;
+  }
+  </style>
+  
